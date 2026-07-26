3a99aea27c9ace381ca1da0cb29e093085422129
Meghana Bhanuprasad <meghanabhanuprasad2468@gmail.com>
2026-07-25 21:17:13 -0400

    Use environment variable for server port

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,4 @@ echo "Running database migrations..."
 alembic upgrade head
 
 echo "Starting server..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
